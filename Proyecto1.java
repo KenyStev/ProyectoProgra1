7b26e81e42a1109a0f555dfb5fbb847060bdc86f
rolandoh <rolandoh96@gmail.com>
2014-10-29 01:43:51 -0600

    Declaracion de variables y arreglos

---
+++ Proyecto1.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Proyecto1{
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+
+		String[] nameProd= {"Azucar","Avena","Trigo","Maiz"};
+		double[] precioProd= {30,25,32,20};
+		int[] kgPorProd=new int[4];
+		String[] logs=new String[10];
+		double[] descuentos={0,0.05,0.1};
+		int[] compYVent=new int[2];
+		int[] starProd=new int[4];
+
+		double caja=0.0;
+		String nameCliente, nameProv, opt;
+		int codeProd, kgCOV, optMenu, indiceDescu=0;
+		double subTotal, total, isv, precioCompra, banco=0.0;
+		boolean isFirstOpen=true;	
+
+	}
+}
